3cf3cadb7684dd7158c685b9a4162c2b00f4f305
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-12 21:44:33 +0530

    dropzone imported

--- src/components/uploaddropzone/uploaddropzone.js
+++ src/components/uploaddropzone/uploaddropzone.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useState } from "react";
-// import { useDropzone } from 'react-dropzone'
-// import { toast } from "react-toastify";
+import { useDropzone } from 'react-dropzone'
+import { toast } from "react-toastify";
 import "./upload.css"
 
 
