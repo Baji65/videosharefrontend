b6c1aa12e85e5bde65dd672173834f68c07c4b8a
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-12 15:24:32 +0530

    related pages added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import {  Nav } from "./components/landingpage/landingpage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signinpage from "./components/signinpage/signinpage";
 import Registerpage from "./components/registerpage/registerpage";
+import { Main } from "./components/mainvideopage/mainvideopage";
+import { Myvideos } from "./components/myvideospage/myvideos";
 
 
 function App() {
   return<>
-  <BrowserRouter>
-  <Routes>
-  <Route path="/" element={<Nav/>}/>
-  <Route path="/login" element={<Signinpage/>}/>
-  <Route path="/register" element={<Registerpage/>}/>
-  </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />} />
+        <Route path="/login" element={<Signinpage />} />
+        <Route path="/register" element={<Registerpage />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/myvideos" element={<Myvideos />} />
+      </Routes>
   
   
   </BrowserRouter>
