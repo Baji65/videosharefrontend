89ca1ab438e580d89c36898e407daba806214d4e
Rajin <rajinradhakrishnan99@gmail.com>
2023-08-12 20:39:29 +0530

    fixed redirecting to main page by clicking back button after logout

--- src/components/mainvideopage/mainvideopage.js
+++ src/components/mainvideopage/mainvideopage.js
@@ -1,5 +1,5 @@
-import React ,{useState}from 'react';
-import { Navigate ,NavLink, useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate, NavLink, useNavigate } from 'react-router-dom';
 import { Landingpage } from '../landingpage/landingpage';
 import "./mainvideo.css";
 import KGF from "../../images/KGF.jpg";
@@ -7,43 +7,51 @@ import { Upload } from '../uploaddropzone/uploaddropzone';
 
 
 
-export function Main(){
-const[visible,setvisible]=useState(false)
-const navigate=useNavigate();
+export function Main() {
+    const [visible, setvisible] = useState(false)
+    const navigate = useNavigate();
 
-const[form,setFormdata]=useState({ name: "",
-video: null,
-description: "",
-category: "",
-visibility: "",
-duration: 10,
-views: 200,})
+    const [form, setFormdata] = useState({
+        name: "",
+        video: null,
+        description: "",
+        category: "",
+        visibility: "",
+        duration: 10,
+        views: 200,
+    })
 
-const handleSubmit=(event)=>{
-    event.preventDefault();
-   
-}
-function signout(){
-    localStorage.removeItem("auth")
-navigate("/")
-}
-    
+    useEffect(()=>{
+        if(!localStorage.getItem("auth")){
+            navigate('/')
+        }
+    },[])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
+    function signout() {
+        localStorage.removeItem("auth")
+        navigate("/")
+    }
+
     return <>
-    <div className='navbar'>
-       <Landingpage/>
-        
-        <div className='link1'>
-        
-        <NavLink to="/myvideos" style={{ "color":'white',"text-decoration": "none"}}><span className='nav1'>My videos</span></NavLink>
-       <span style={{ "color":'white',"padding":"10px" }}>|</span>
-      <button className='nav1' onClick={()=>setvisible(true)}>Upload</button>
-        <span style={{ "color":'white',"padding":"10px" }}>|</span>
-        <button className='nav1' onClick={signout}>Sign out</button>
-    </div>
-    </div>
-    <div >
-    <img  className='image1' src={KGF} alt=' not found'/>
-    </div>
-    <Upload form={form} setFormdata={setFormdata} handleSubmit={handleSubmit} visible={visible} setvisible={setvisible}/>
+        <div className='navbar'>
+            <Landingpage />
+
+            <div className='link1'>
+
+                <NavLink to="/myvideos" style={{ "color": 'white', "text-decoration": "none" }}><span className='nav1'>My videos</span></NavLink>
+                <span style={{ "color": 'white', "padding": "10px" }}>|</span>
+                <button className='nav1' onClick={() => setvisible(true)}>Upload</button>
+                <span style={{ "color": 'white', "padding": "10px" }}>|</span>
+                <button className='nav1' onClick={signout}>Sign out</button>
+            </div>
+        </div>
+        <div >
+            <img className='image1' src={KGF} alt=' not found' />
+        </div>
+        <Upload form={form} setFormdata={setFormdata} handleSubmit={handleSubmit} visible={visible} setvisible={setvisible} />
     </>
 }
